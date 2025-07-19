1a94fd33a4fc747d1fa77c820ccc4400ba4f87cf
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-19 22:54:53 +0530

    Add additional chalk messages for enhanced console output

--- index.mjs
+++ index.mjs
@@ -4,3 +4,5 @@ console.log(chalk.blue("Hello, World!"));
 console.log(chalk.green("This is a green message!"));
 console.log(chalk.red("This is a red message!"));
 console.log(chalk.yellow("This is a yellow message!"));
+console.log(chalk.bgCyan("This is a message with a cyan background!"));
+console.log(chalk.bold("This is a bold message!"));

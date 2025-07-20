cb5ee11a22e3ab9e508803a5d16e8afe73c94ec3
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-20 23:27:37 +0530

    Remove index.mjs and update start script to use index.js; add __dirname logging to index.js

--- index.mjs
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-
-console.log(chalk.blue("Hello, World!"));
-console.log(chalk.green("This is a green message!"));
-console.log(chalk.red("This is a red message!"));
-console.log(chalk.yellow("This is a yellow message!"));
-console.log(chalk.bgCyan("This is a message with a cyan background!"));
-console.log(chalk.bold("This is a bold message!"));

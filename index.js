f0ff03d5c76dd47f5318b73198212828a171b890
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-21 23:17:14 +0530

    Refactor index.js to implement CLI functionality using commander; add a.txt with sample greetings

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log(__dirname);
+const { command } = require("commander");
+const program = new command();
+
+function main() {
+  program
+    .name("my-cli")
+    .description("A simple CLI application")
+    .version("1.0.0");
+
+  program
+    .command("greet <name>")
+    .description("Greet a user by name")
+    .action((name) => {
+      console.log(`Hello, ${name}!`);
+    });
+
+  program.parse(process.argv);
+}
+
+main();

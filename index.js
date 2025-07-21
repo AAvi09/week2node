090e1232f905f1ce307117c46a7aa818370c378e
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-21 23:26:05 +0530

    Refactor index.js to implement word counting functionality in CLI tool

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+const fs = require("fs");
 const { command } = require("commander");
 const program = new command();
 
 function main() {
   program
-    .name("my-cli")
-    .description("A simple CLI application")
+    .name("count")
+    .description("A simple CLI tool to count words in a file")
     .version("1.0.0");
 
   program
-    .command("greet <name>")
-    .description("Greet a user by name")
-    .action((name) => {
-      console.log(`Hello, ${name}!`);
+    .command("count_words <file>")
+    .description("Count the number of words in a file")
+    .action((file) => {
+      fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+          console.error(`Error reading file: ${err.message}`);
+          return;
+        }
+        const words = data.split(/\s+/).filter((word) => word.length > 0);
+        console.log(`The file contains ${words.length} words.`);
+      });
     });
 
   program.parse(process.argv);
